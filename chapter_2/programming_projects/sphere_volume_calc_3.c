fb8fdf0bbb63324d6c034f52c2cab9fad3e84142
Montell <shobax1@gmail.com>
2024-01-02 20:39:37 -0800

    create a program that prompt a user for radius and calculates volume of a sphere

---
+++ chapter_2/programming_projects/sphere_volume_calc_3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#define PI 3.14159f
+
+/**
+ * main - calculates volume of a sphere and take user input
+ * Return: nothing
+ */
+
+int main(void)
+{
+	float r = 0;
+	float v = 0;
+
+	scanf("%f", &r);
+	v = 4.0f / 3.0f * PI * r * r * r;
+	printf("Volume of sphere is: %f\n", v);
+
+	return (0);
+}
