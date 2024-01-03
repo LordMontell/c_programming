49f571f2ccfbd239d11daacba776d1ace4ad75cd
Montell <shobax1@gmail.com>
2024-01-02 20:16:18 -0800

    define the value of a pie and replace the magic number

--- chapter_2/programming_projects/sphere_volume_calc.c
+++ chapter_2/programming_projects/sphere_volume_calc.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#define PI	3.14159
 /**
   * main - calculates a volume of a sphere
   * Return: nothing
@@ -10,7 +10,7 @@ int main(void)
 	float r = 10.0f;
 	float v = 0;
 
-	v = 4.0f / 3.0f * 3.14 * r * r * r;
+	v = 4.0f / 3.0f * PI * r * r * r;
 	printf("Volume of sphere is: %f\n", v);
 
 	return (0);
