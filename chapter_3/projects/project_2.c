a52b36d3e20bb42f007397d8788576d7b56f738b
Montell <shobax1@gmail.com>
2024-01-27 04:40:05 -0800

    Minimize the lines of code

--- chapter_3/projects/project_2.c
+++ chapter_3/projects/project_2.c
@@ -22,9 +22,6 @@ int main(void)
 	printf("Enter purchase date (mm/dd/yyyy): ");
 	scanf("%d/%d/%d", &month, &day, &year);
 
-	printf("Item\t\tUnit\t\tpurchase\n");
-	printf("\t\tprice");
-	printf("\t\tdate\n");
-
+	printf("Item\t\tUnit\t\tpurchase\n\t\tprice\t\tdate\n");
 	printf("%d\t\t$%7.2f\t%d/%d/%d\n", item_no, unit_price, month, day, year);
 }
