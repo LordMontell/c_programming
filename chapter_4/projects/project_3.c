d6830f7bc7e326401635d1c906d793b061782f0b
Montell <shobax1@gmail.com>
2024-01-27 06:12:35 -0800

    Enforce scanf to read single decimal digit

--- chapter_4/projects/project_3.c
+++ chapter_4/projects/project_3.c
@@ -12,7 +12,7 @@ int main(void)
 	int num1, num2, num3;
 
 	printf("Enter a three-digit number: ");
-	scanf("%d%d%d", &num1, &num2, &num3);
+	scanf("%1d%1d%1d", &num1, &num2, &num3);
 
 	printf("The reversal is: %d%d%d\n", num3, num2, num1);
 
