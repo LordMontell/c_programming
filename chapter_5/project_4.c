2d075e0ad0ae2ffd8ff2f2682b34bb9b0eebab87
Montell <shobax1@gmail.com>
2024-02-02 02:02:15 +0200

    Write a program that asks the user to enter a wind speed (in knots), then displays the corresponding description.

---
+++ chapter_5/project_4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - asks the user to enter a wind speed (in knots),then
+ * displays the corresponding description
+ * Return: (0)
+ */
+
+int main(void)
+{
+	int speed;
+
+	scanf("%d", &speed);
+
+	if (speed < 1)
+		printf("Calm\n");
+	else if (speed <= 3)
+		printf("Light air\n");
+	else if (speed <= 27)
+		printf("Breeze\n");
+	else if (speed <= 47)
+		printf("Gale\n");
+	else if (speed <= 63)
+		printf("Storm\n");
+	else if (speed > 63)
+		printf("Hurricane\n");
+
+	return (0);
+}
