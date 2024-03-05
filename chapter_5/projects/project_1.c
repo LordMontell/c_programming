6c53d8bdce84b0d77a534ad7855d49a1d79550c8
Montell <shobax1@gmail.com>
2024-03-05 18:47:45 +0200

    Remove dith field and consider negative numbers in conditions

--- chapter_5/projects/project_1.c
+++ chapter_5/projects/project_1.c
@@ -7,19 +7,19 @@
 
 int main(void)
 {
-	int digit;
+	int n;
 
 	printf("Enter a number: ");
-	scanf("%4d", &digit);
+	scanf("%d", &n);
 
-	if (digit >= 0 && digit <= 9)
-		printf("The number %d has 1 digits\n", digit);
-	else if (digit >= 10 && digit <= 99)
-		printf("The number %d has 2 digits\n", digit);
-	else if (digit >= 100 && digit <= 999)
-		printf("The number %d has 3 digits\n", digit);
-	else if (digit >= 1000 && digit <= 9999)
-		printf("The number %d has 4 digits\n", digit);
+	if ((n >= -9 && n <= 0) || (n >= 0 && n <= 9))
+		printf("The number %d has 1 digits\n", n);
+	else if ((n <= -10 && n >= -99) || (n >= 10 && n <= 99))
+		printf("The number %d has 2 digits\n", n);
+	else if ((n <= -100 && n >= -999) || (n >= 100 && n <= 999))
+		printf("The number %d has 3 digits\n", n);
+	else if ((n <= -1000 && n >= -9999) || (n >= 1000 && n <= 9999))
+		printf("The number %d has 4 digits\n", n);
 
 	return (0);
 }
